7142e6e25c1a12e8f8f9ad6cd67e2989934a98b1
Giorgi Gabritchidze <127999860+Gabysia@users.noreply.github.com>
2023-12-20 21:44:41 +0400

    first commit

---
+++ src/app/components/TodoPage/page.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+
+interface Todo {
+  id: number;
+  text: string;
+}
+
+const TodoPage: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  useEffect(() => {
+    // Load todos from local storage when component mounts
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Save todos to local storage whenever todos state changes
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (inputValue.trim() === '') {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: new Date().getTime(),
+      text: inputValue,
+    };
+
+    setTodos([...todos, newTodo]);
+    setInputValue('');
+  };
+
+  const handleDelete = (id: number) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={inputValue} onChange={handleChange} />
+        <button type='submit'>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoPage;
